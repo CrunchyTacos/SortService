41429270eee894d2e5ee367de0ab485fd47d326c
David Deering <David Deering>
2016-02-11 09:09:54 -0600

    the real commit

---
+++ src/SortService/Controllers/SortController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json.Linq;
+using SortService.Models;
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SortService.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class SortController : Controller
+    {
+
+        [FromServices]
+        public ISortRepository sortArray { get; set; }
+
+        // POST api/sort
+        [HttpPost]
+        public IActionResult Submit([FromBody]SortItem item)
+        {
+            if (item == null)
+            {
+                return HttpBadRequest();
+            }
+            sortArray.sort(item);
+            return new ObjectResult(item);
+        }
+    }
+}
---
+++ src/SortService/Models/SortRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SortService.Models
+{
+    public class SortRepository : ISortRepository
+    {
+        public void sort(SortItem item)
+        {
+            Array.Sort(item.arr);
+        }
+    }
+}
